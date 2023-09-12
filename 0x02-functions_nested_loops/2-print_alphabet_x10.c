af02b3e5249dc27206f6b590e9f22b9ca42a3b77
Aishagojo <gojoaisha@gmail.com>
2023-09-12 12:29:43 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10x in lower case
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+
+	int y;
+
+	for (y = 1; y <= 10; y++)
+	{
+		for (i = 'a' ; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
+
